c56c66e8ee20eced3e1a2ba030f343574097cff7
GabR3924 <garc2004@gmail.com>
2024-10-22 10:51:15 -0400

    tasa dias vista

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -6,6 +6,44 @@ const Inicio = () => {
   const [fechaHora, setFechaHora] = useState(new Date());
   const [datos, setDatos] = useState([]);
   const [datosColegios, setDatosColegios] = useState([]);
+  const [tasaDia, setTasaDia] = useState("");
+
+  useEffect(() => {
+    // Hacer la petición a la API para obtener los datos
+    axios
+      .get("https://apidev.gocastgroup.com/api/tasa_dia.php")
+      .then((response) => {
+        console.log("Datos recibidos:", response.data);
+        setTasaDia(response.data);  // Almacenar todos los datos recibidos
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos:", error);
+      });
+
+    // Limpiar el temporizador cuando se desmonte el componente
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    // Configurar el temporizador para actualizar la fecha y la hora cada segundo
+    const timer = setInterval(() => {
+      setFechaHora(new Date());
+    }, 1000);
+
+    // Hacer la petición a la API para obtener los datos
+    axios
+      .get("https://rcv.gocastgroup.com:2053/vivirseguros/colegios-datos")
+      .then((response) => {
+        console.log("Datos recibidos:", response.data);
+        setDatosColegios(response.data);  // Almacenar todos los datos recibidos
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos:", error);
+      });
+
+    // Limpiar el temporizador cuando se desmonte el componente
+    return () => clearInterval(timer);
+  }, []);
 
   useEffect(() => {
     // Configurar el temporizador para actualizar la fecha y la hora cada segundo
@@ -86,83 +124,6 @@ const Inicio = () => {
     link.click();
     document.body.removeChild(link);
   };
-  
-  useEffect(() => {
-    // Configurar el temporizador para actualizar la fecha y la hora cada segundo
-    const timer = setInterval(() => {
-      setFechaHora(new Date());
-    }, 1000);
-
-    // Hacer la petición a la API para obtener los datos
-    axios
-      .get("https://rcv.gocastgroup.com:2053/vivirseguros/colegios-datos")
-      .then((response) => {
-        console.log("Datos recibidos:", response.data);
-        setDatosColegios(response.data);  // Almacenar todos los datos recibidos
-      })
-      .catch((error) => {
-        console.error("Error al obtener los datos:", error);
-      });
-
-    // Limpiar el temporizador cuando se desmonte el componente
-    return () => clearInterval(timer);
-  }, []);
-
-  // const generarReporteColegios = () => {
-  //   // Verificar que los datos estén en la estructura esperada
-  //   if (!datosColegios || datosColegios.length === 0) {
-  //     console.log("No hay datos disponibles para generar el reporte.");
-  //     return;
-  //   }
-
-  //   const alumno = datosColegios.alumno || []; // Asegúrate de que `alumno` esté definido
-  //   const pagos_alumnos = datosColegios.pagos_alumnos || []; // Asegúrate de que `pagos_alumnos` esté definido
-
-  //   if (alumno.length === 0 || pagos_alumnos.length === 0) {
-  //     console.log("No hay suficientes datos de alumno o pagos para generar el reporte.");
-  //     return;
-  //   }
-
-  //   // Creación del CSV
-  //   const csvData = [
-  //     [
-  //       "Cédula", "Nombre", "Apellido", "Fecha de Nacimiento", "Teléfono", 
-  //       "Correo", "Estado", "Dirección", "Referencia Pago", "Monto Pago", 
-  //       "Banco Pago", "Plan"
-  //     ],
-  //     ...alumno.map((alumno, index) => {
-  //       const pago = pagos_alumnos[index] || {}; // Asegúrate de que haya un pago para este alumno
-  
-  //       return [
-  //         alumno.cedula || "",
-  //         alumno.nombre || "",
-  //         alumno.apellido || "",
-  //         new Date(alumno.fecha_nacimiento).toLocaleDateString() || "",
-  //         alumno.telefono || "",
-  //         alumno.correo || "",
-  //         alumno.estado || "",
-  //         alumno.direccion || "",
-  //         pago.referencia || "",
-  //         pago.monto || "",
-  //         pago.banco || "",
-  //         pago.plan || ""
-  //       ];
-  //     })
-  //   ];
-
-  //   // Generación del archivo CSV
-  //   const csvContent = "data:text/csv;charset=utf-8," 
-  //     + csvData.map(e => e.join(";")).join("\n");
-
-  //   const encodedUri = encodeURI(csvContent);
-  //   const link = document.createElement("a");
-  //   link.setAttribute("href", encodedUri);
-  //   link.setAttribute("download", "reporte_completo.csv");
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  // };
-  
 
   const generarReporteColegios = () => {
     // Verificar que los datos estén en la estructura esperada
@@ -226,6 +187,10 @@ const Inicio = () => {
       <div className="saludo-container">
         <h1>¡Hola! Bienvenido</h1>
         <p>{fechaHora.toLocaleString()}</p>
+        <h3>Tasa del dia
+          <br />
+          <span>{tasaDia}</span>
+        </h3>
       </div>
 
       <div className="reportes-container">
