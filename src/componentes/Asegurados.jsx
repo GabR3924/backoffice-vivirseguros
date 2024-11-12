b76fadba5e4d9fabc70e842b49a2ba9defeafa40
GabR3924 <garc2004@gmail.com>
2024-11-12 16:10:19 -0400

    ok asegurados

--- src/componentes/Asegurados.jsx
+++ src/componentes/Asegurados.jsx
@@ -5,6 +5,7 @@ import "../CSS/Asegurados.css";
 export default function Asegurados() {
   const [asegurados, setAsegurados] = useState([]);
   const [aseguradosMostrados, setAseguradosMostrados] = useState([]);
+  const [pagosMostrados, setPagosMostrados] = useState([]);
   const [fechaFiltro, setFechaFiltro] = useState("");
   const [vehiculos, setVehiculos] = useState([]); // Estado para los datos de vehículos
 
@@ -15,6 +16,7 @@ export default function Asegurados() {
         console.log("asegurados", response.data);
         setAsegurados(response.data.asegurados);
         setAseguradosMostrados(response.data.asegurados);
+        setPagosMostrados(response.data.pagos);
         setVehiculos(response.data.vehiculos); // Guardar los datos de vehículos
       })
       .catch((error) => {
@@ -52,14 +54,10 @@ export default function Asegurados() {
     <div className="asegurados">
       <h2>Asegurados</h2>
       <div className="filtro-fecha">
-        <input
-          type="date"
-          value={fechaFiltro}
-          onChange={handleFechaFiltroChange}
-        />
+        <input type="date" value={fechaFiltro} onChange={handleFechaFiltroChange} />
         <button onClick={() => setFechaFiltro("")}>Limpiar Filtro</button>
       </div>
-
+  
       {aseguradosMostrados.map((asegurado) => (
         <div key={asegurado.id} className="asegurado">
           <p className="fecha">
@@ -68,30 +66,99 @@ export default function Asegurados() {
           <button onClick={() => handleToggleInfo(asegurado.id)}>
             {asegurado.expanded ? "Ocultar Detalles" : "Mostrar Detalles"}
           </button>
+  
           {asegurado.expanded && (
             <div className="info-adicional">
-              <p><strong>Nombre:</strong> {asegurado.nombre_propietario}</p>
-              <p><strong>Apellido:</strong> {asegurado.apellido_propietario}</p>
-              <p><strong>Cédula:</strong> {asegurado.cedula_propietario}</p>
-              <p><strong>Correo:</strong> {asegurado.correo}</p>
-              <p><strong>Fecha de nacimiento:</strong> {new Date(asegurado.fecha_nacimiento).toLocaleDateString()}</p>
-              <p><strong>Teléfono:</strong> {asegurado.telefono}</p>
-              <p><strong>Ciudad:</strong> {asegurado.ciudad}</p>
-              <p><strong>Estado:</strong> {asegurado.estado}</p>
-              <p><strong>Municipio:</strong> {asegurado.municipio}</p>
-              <p><strong>Dirección:</strong> {asegurado.direccion}</p>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Campo</th>
+                    <th>Información</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Nombre</td>
+                    <td>{asegurado.nombre_propietario}</td>
+                  </tr>
+                  <tr>
+                    <td>Apellido</td>
+                    <td>{asegurado.apellido_propietario}</td>
+                  </tr>
+                  <tr>
+                    <td>Cédula</td>
+                    <td>{asegurado.cedula_propietario}</td>
+                  </tr>
+                  <tr>
+                    <td>Correo</td>
+                    <td>{asegurado.correo}</td>
+                  </tr>
+                  <tr>
+                    <td>Fecha de nacimiento</td>
+                    <td>{new Date(asegurado.fecha_nacimiento).toLocaleDateString()}</td>
+                  </tr>
+                  <tr>
+                    <td>Teléfono</td>
+                    <td>{asegurado.telefono}</td>
+                  </tr>
+                  <tr>
+                    <td>Ciudad</td>
+                    <td>{asegurado.ciudad}</td>
+                  </tr>
+                  <tr>
+                    <td>Estado</td>
+                    <td>{asegurado.estado}</td>
+                  </tr>
+                  <tr>
+                    <td>Municipio</td>
+                    <td>{asegurado.municipio}</td>
+                  </tr>
+                  <tr>
+                    <td>Dirección</td>
+                    <td>{asegurado.direccion}</td>
+                  </tr>
+                </tbody>
+              </table>
 
-              {/* Mostrar imagen con enlace para descargar */}
+              <h4>Pagos:</h4>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Plan</th>
+                    <th>Prima Básica</th>
+                    <th>Prima Total</th>
+                    <th>Suma Cosas</th>
+                    <th>Suma Personas</th>
+                    <th>Coberturas Adicionales</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pagosMostrados
+                    .filter((pago) => pago.id_asegurado === asegurado.id)
+                    .map((pago) => (
+                      <tr key={pago.id}>
+                        <td>{pago.plan}</td>
+                        <td>{pago.prima_basica_dlrs}</td>
+                        <td>{pago.prima_total_dlrs}</td>
+                        <td>{pago.suma_cosas_dlrs}</td>
+                        <td>{pago.suma_personas_dlrs}</td>
+                        <td>{pago.extra_plan}</td>
+               
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+  
               {asegurado.imagen_cedula && (
                 <div>
                   <p><strong>Imagen Cédula:</strong></p>
                   <img src={asegurado.imagen_cedula} alt="Imagen Cédula" style={{ width: "100px", height: "auto" }} />
-                  <img src={asegurado.imagen_rif} alt="Imagen Rif" style={{ width: "100px", height: "auto" }} />
-                  <button onClick={() => handleImageDownload(asegurado.imagen_cedula)}>Descargar Imagen</button>
+                  {/* <button onClick={() => handleImageDownload(asegurado.imagen_cedula)}>
+                    Descargar Imagen
+                  </button> */}
                 </div>
               )}
-
-              {/* Mostrar la información de vehículos */}
+  
               <h4>Vehículos:</h4>
               {vehiculos
                 .filter((vehiculo) => vehiculo.id_asegurado === asegurado.id)
@@ -99,14 +166,18 @@ export default function Asegurados() {
                   <div key={vehiculo.id}>
                     <p><strong>Carnet de Circulación:</strong></p>
                     <img src={vehiculo.imagen_vehiculo} alt="Imagen Vehículo" style={{ width: "100px", height: "auto" }} />
-                    <img src={vehiculo.imagen_propiedad} alt="Imagen propiedad" style={{ width: "100px", height: "auto" }} />
-                    <button onClick={() => handleImageDownload(vehiculo.imagen_vehiculo)}>Descargar Imagen</button>
+                    {/* <button onClick={() => handleImageDownload(vehiculo.imagen_vehiculo)}>
+                      Descargar Imagen
+                    </button> */}
                   </div>
                 ))}
+  
+          
             </div>
           )}
         </div>
       ))}
     </div>
   );
+  
 }
